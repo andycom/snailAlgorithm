13e70b0dcdb087aab6d38047a0b5b8ed801934fc
andy <winglive@163.com>
2021-03-25 09:39:07 +0800

    feature ：132 math

--- src/main/java/com/fancv/leetCode/mathematics/MyMathematics.java
+++ src/main/java/com/fancv/leetCode/mathematics/MyMathematics.java
@@ -7,22 +7,23 @@ public class MyMathematics {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(findErrorNums(new int[]{1, 1})));
+        System.out.println(Arrays.toString(findErrorNums(new int[]{1, 2, 2})));
+        System.out.println(Arrays.toString(findErrorNums(new int[]{1, 2, 3, 5, 4, 5})));
+        System.out.println(Arrays.toString(findErrorNums(new int[]{1, 2, 3, 2, 4, 5, 6, 7, 8, 9})));
     }
 
     public static int[] findErrorNums(int[] nums) {
         int r[] = {0, 0};
         Arrays.sort(nums);
-
         for (int i = 0; i < nums.length; i++) {
             int len = (i + 1) < nums.length - 1 ? i + 1 : nums.length - 1;
-            if (nums[i] == nums[len]) {
+            if (r[0] == 0 && nums[i] == nums[len]) {
                 r[0] = nums[i];
             }
-            if (nums.length==2) {
-                r[1] =
+            if (r[1] == 0 && nums[i] != i + 1 && nums[len] != i + 1) {
+                r[1] = i + 1;
             }
         }
-
         return r;
     }
 
