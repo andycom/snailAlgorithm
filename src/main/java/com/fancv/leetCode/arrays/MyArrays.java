d6157312b2ff19e04aa2522821dfdf7f58051195
andy <winglive@163.com>
2021-03-27 15:59:18 +0800

    feature ：避免洪水DONE

--- src/main/java/com/fancv/leetCode/arrays/MyArrays.java
+++ src/main/java/com/fancv/leetCode/arrays/MyArrays.java
@@ -1,40 +1,47 @@
 package com.fancv.leetCode.arrays;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeSet;
 
 public class MyArrays {
     public static void main(String[] args) {
-        int[] rains = {1, 2, 0, 0, 2, 4};
-        avoidFlood(rains);
+        int[] rains = {1, 0, 2, 0, 3, 0, 2, 0, 0, 0, 1, 2, 3};
+        System.out.println(Arrays.toString(avoidFlood(rains)));
     }
 
     public static int[] avoidFlood(int[] rains) {
         int ans[] = new int[rains.length];
-
-        int zero = 0;
         Map<Integer, Integer> r = new HashMap<>();
+        //存储可以 rains[i] == 0 的索引位置（这里使用 TreeSet 是为了方便查找 适合 排空湖泊 的索引天数）
+        TreeSet<Integer> set = new TreeSet<>();
+
         for (int i = 0; i < rains.length; i++) {
             if (rains[i] > 0) {
-                if (r.get(rains[i]) == null) {
-                    r.put(rains[i], i);
-                } else {
+                if (r.containsKey(rains[i])) {
+                    //查出第几天哪个池塘下过雨
                     int reained = r.get(rains[i]);
-                    for (int j = 0; j < i; j++) {
-                        if (ans[j] == 0) {
-                            ans[j] = reained;
-                            r.put(rains[i], i);
-                        }
+                    Integer zeroIdx = set.higher(reained);
+                    if (zeroIdx == null) {
+                        int a[] = {};
+                        return a;
                     }
+                    ans[zeroIdx] = rains[i];
+                    set.remove(zeroIdx);
                 }
+                r.put(rains[i], i);
                 ans[i] = -1;
             } else {
-                zero++;
-                ans[i] = 0;
+                set.add(i);
             }
         }
 
-
+        for (int i = 0; i < ans.length; i++) {
+            if (ans[i] == 0) {
+                ans[i] = 1;
+            }
+        }
         return ans;
     }
 }
--- src/main/java/com/fancv/leetCode/mathematics/MathArray.java
+++ src/main/java/com/fancv/leetCode/mathematics/MathArray.java
@@ -10,6 +10,18 @@ public class MathArray {
 
     public static void main(String[] args) {
 
+
+        int a[] = {1, 1, 1, 2, 3, 10, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 7, 8, 9, 10, 11, 12, 13, 14};
+        int b[] = {2, 1, 2, 2, 3, 1, 0, 0, 9, 5, 5, 5, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 3};
+        int c[] = {1, 3, 2, 2, 10, 4, 2, 3, 5, 9};
+        int d[] = {1, 3, 3, 10, 4, 2, 3, 5};
+
+
+        System.out.println(findLengthOfShortestSubarray(a));
+        System.out.println(findLengthOfShortestSubarray(b));
+        System.out.println(findLengthOfShortestSubarray(c));
+        System.out.println(findLengthOfShortestSubarray(d));
+
     }
 
     public int maximumGap(int[] nums) {
@@ -20,8 +32,58 @@ public class MathArray {
         //2.遍历元素，计算相邻元素差值 最大
 
 
-
         return 0;
+    }
+
+    /**
+     * [1,12,3,10,4,2,3,5]  非递减
+     * <p>
+     * 1.特殊情况
+     * 2.循环遍历 i
+     *
+     * @param arr
+     * @return
+     */
+    public static int findLengthOfShortestSubarray(int[] arr) {
+        int r = 0;
+        int len = arr.length;
+        int tail = -1;
+        //1.判断空 和length  为1 的情况
+        if (arr == null || len == 1) {
+            return r;
+        }
+        // 1.循环遍历
+        int temp = -1;
+        for (int i = 0; i < len; i++) {
+            //2.相等后移
+            if (temp < 0) {
+                temp = arr[i];
+            }
+            if (arr[i] == temp) {
+                continue;
+            }
+            int head = -1;
+            int sum = 0;
+            int min = -1;
+            for (int j = i + 1; j < len; j++) {
+                if (arr[j] < temp && arr[j] > min) {
+                    if (head < 0) {
+                        head = j;
+                    }
+                    sum++;
+                }
+            }
+            if (head < sum) {
+                r = r + head;
+            } else {
+                r = r + sum;
+
+            }
+
+
+        }
+
+        return r;
     }
 
 }
